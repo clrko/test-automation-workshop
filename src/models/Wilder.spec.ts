ad803f56cb55d845733b695f05bcecfdc5546d2a
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-12-15 11:06:01 +0100

    Return city and name in getDisplayName

---
+++ src/models/Wilder.spec.ts
@@ -0,0 +1,27 @@
+import { getDisplayName } from './Wilder';
+
+describe('getDisplayName', () => {
+  describe('when not passed city', () => {
+    it('returns question mark and full name', () => {
+      expect(getDisplayName('Lucie', 'Laforêt')).toEqual('[?] Lucie Laforêt');
+    });
+  });
+
+  describe('when passed city', () => {
+    describe('when passed city without custom code', () => {
+      it('returns city and full name', () => {
+        expect(getDisplayName('Lucie', 'Laforêt', 'Marseille')).toEqual(
+          '[Marseille] Lucie Laforêt'
+        );
+      });
+    });
+
+    describe('when passed city with custom code', () => {
+      it('returns city code and full name', () => {
+        expect(getDisplayName('Lucie', 'Laforêt', 'Paris')).toEqual(
+          '[PAR] Lucie Laforêt'
+        );
+      });
+    });
+  });
+});
